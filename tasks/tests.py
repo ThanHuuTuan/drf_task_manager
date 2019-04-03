222771d78eb1f4ff7c12f25d80dfbe470dfd086d
p.savitski <pavel.savitski@gmail.com>
2019-04-03 13:03:33 +0300

    simple tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.utils import timezone
+
+from projects.models import Project
+from tasks.models import Task
+from users.models import User
+
+
+class TaskModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create(username='test_name', email='email@mail.com', password='123qweasd')
+        project = Project.objects.create(title='Writing tests')
+        Task.objects.create(title='Create test', due_date=timezone.now(), project=project, assigned_user=user)
+
+    def test_task_description_label(self):
+        task = Task.objects.get(id=1)
+        field_label = task._meta.get_field('description').verbose_name
+        self.assertEquals(field_label, 'Description')
+
+    def test_task_title_max_length(self):
+        task = Task.objects.get(id=1)
+        max_length = task._meta.get_field('title').max_length
+        self.assertEquals(max_length, 255)
+
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+from users.models import User
+from users.serializers import UserSerializer, UserInfoSerializer
+
+
+class APITUserTest(TestCase):
+    def setUp(self):
+        self.user_attributes = {
+            'username': 'apitest',
+            'password': '123qweasd',
+            'email': 'test@mail.com'
+        }
+
+        self.user = User.objects.create(**self.user_attributes)
+        self.serializer = UserInfoSerializer(instance=self.user)
+
+    def test_contains_expected_fields(self):
+        data = self.serializer.data
+
+        self.assertEqual(set(data.keys()), set(['id', 'email', 'username', 'role']))
